617b74e4ed4bc681df7a4772cb61d6312bb645ef
gy <misschuer@163.com>
2017-07-14 11:58:58 +0800

    增加应用服的socket链接

---
+++ src/main/java/cc/mi/app/Startup.java
@@ -0,0 +1,38 @@
+package cc.mi.app;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.ini4j.Config;
+import org.ini4j.Ini;
+import org.ini4j.Profile.Section;
+
+import cc.mi.app.appClient.AppClient;
+
+public class Startup {
+	private static final String APP_CLIENT = "appClient";
+	private static final String IP = "ip";
+	private static final String PORT = "port";
+	
+	private static void loadConfig() throws NumberFormatException, Exception {
+		Config cfg = new Config();
+		URL url = Startup.class.getResource("/config.ini");
+		Ini ini = new Ini();
+        ini.setConfig(cfg);
+        try {
+        	// 加载配置文件  
+        	ini.load(url);
+
+        	Section section = ini.get(APP_CLIENT);
+        	AppClient.start(section.get(IP), Integer.parseInt(section.get(PORT)));
+        	
+        } catch (IOException e) {
+        	e.printStackTrace();
+	    }  
+	}
+
+	public static void main(String[] args) throws NumberFormatException, Exception {
+		loadConfig();
+	}
+
+}
