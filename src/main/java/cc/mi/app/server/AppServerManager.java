7410ea1dcfd4ea91bbe37c21128f5cdfac972127
gong <403575226@qq.com>
2018-07-02 20:52:28 +0800

    往中心服注册消息

--- src/main/java/cc/mi/app/server/AppServerManager.java
+++ src/main/java/cc/mi/app/server/AppServerManager.java
@@ -1,10 +1,26 @@
 package cc.mi.app.server;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import cc.mi.core.constance.IdentityConst;
+import cc.mi.core.handler.Handler;
 import cc.mi.core.manager.ServerManager;
 
 public class AppServerManager extends ServerManager {
 	private static AppServerManager instance;
+	// 消息收到以后的回调
+	private static final Map<Integer, Handler> handlers = new HashMap<>();
+	private static final List<Integer> opcodes;
+	
+	static {
+		
+		opcodes = new LinkedList<>();
+		opcodes.addAll(handlers.keySet());
+	}
+	
 	
 	public static AppServerManager getInstance() {
 		if (instance == null) {
@@ -14,6 +30,6 @@ public class AppServerManager extends ServerManager {
 	}
 	
 	public AppServerManager() {
-		super(IdentityConst.SERVER_TYPE_APP);
+		super(IdentityConst.SERVER_TYPE_APP, opcodes);
 	}
 }
