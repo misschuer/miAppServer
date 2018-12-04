49c2489b3c061f2273155cdf55aec2058302f20c
Your Name <you@example.com>
2018-12-04 17:43:16 +0800

    修改消息包名称

--- src/main/java/cc/mi/app/handler/PlayerLoginHandler.java
+++ src/main/java/cc/mi/app/handler/PlayerLoginHandler.java
@@ -2,7 +2,7 @@ package cc.mi.app.handler;
 
 import cc.mi.app.server.AppServerManager;
 import cc.mi.core.constance.LoginActionEnum;
-import cc.mi.core.generate.msg.PlayerLoginMsg;
+import cc.mi.core.generate.msg.AppPlayerLoginMsg;
 import cc.mi.core.handler.HandlerImpl;
 import cc.mi.core.packet.Packet;
 import cc.mi.core.server.ServerContext;
@@ -12,7 +12,7 @@ public class PlayerLoginHandler extends HandlerImpl {
 
 	@Override
 	public void handle(ServerContext nil, Channel channel, Packet decoder) {
-		PlayerLoginMsg packet = (PlayerLoginMsg) decoder;
+		AppPlayerLoginMsg packet = (AppPlayerLoginMsg) decoder;
 		AppServerManager.getInstance().loginManager.pushAction(packet.getGuid(), packet.getClientFd(), LoginActionEnum.CONTEXT_LOGIN_ACTION_LOGIN);
 	}
 
